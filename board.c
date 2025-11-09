455db6699865f5eb56b1215f21772fa9329ea5ad
Alexander Szura <aslszura@gmail.com>
2025-11-09 17:52:57 -0500

    Setup & Board Initialization
    -Created a tools file for useful functions, definitions, and debugging
    -Implemented board_initialize()

--- board.c
+++ board.c
@@ -1,4 +1,6 @@
 #include "board.h"
+#include "tools.h"
+#include "stdio.h"
 
 const char *player_string(enum chess_player player)
 {
@@ -8,6 +10,17 @@ const char *player_string(enum chess_player player)
         return "white";
     case PLAYER_BLACK:
         return "black";
+    }
+}
+
+char player_char(enum chess_player player)
+{
+    switch (player)
+    {
+    case PLAYER_WHITE:
+        return 'w';
+    case PLAYER_BLACK:
+        return 'b';
     }
 }
 
@@ -30,10 +43,83 @@ const char *piece_string(enum chess_piece piece)
     }
 }
 
+char piece_char(enum chess_piece piece)
+{
+    switch (piece)
+    {
+    case PIECE_PAWN:
+        return 'p';
+    case PIECE_KNIGHT:
+        return 'n';
+    case PIECE_BISHOP:
+        return 'b';
+    case PIECE_ROOK:
+        return 'r';
+    case PIECE_QUEEN:
+        return 'q';
+    case PIECE_KING:
+        return 'k';
+    }
+}
+
+const char *color_string(enum chess_player color)
+{
+    switch (color)
+    {
+    case PLAYER_BLACK:
+        return "BLACK";
+    case PLAYER_WHITE:
+        return "WHITE";
+    default:
+        return "UNKNOWN";
+    }
+}
+
 void board_initialize(struct chess_board *board)
 {
     board->next_move_player = PLAYER_WHITE;
-    // TODO: initialize the board state for a new chess game.
+    
+    board->white_can_castle = true;
+    board->black_can_castle = true;
+
+    for (int i = 0; i < BOARD_SIZE; i++)
+        board->piece_present[i] = false;
+
+    /* --------------- Setup Pawns --------------- */
+    int white_cord = from_cords(0, 1);
+    int black_cord = from_cords(0, GRID_SIZE - 2);
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        board->piece_id[white_cord] = PIECE_PAWN;
+        board->piece_id[black_cord] = PIECE_PAWN;
+
+        board->piece_color[white_cord] = PLAYER_WHITE;
+        board->piece_color[black_cord] = PLAYER_BLACK;
+
+        board->piece_present[white_cord++] = true;
+        board->piece_present[black_cord++] = true;
+    }
+    /* --------------- Setup Pawns --------------- */
+
+    /* --------------- Bottom Rank --------------- */
+
+    enum chess_piece bottom_rank[] = {PIECE_ROOK, PIECE_KNIGHT, PIECE_BISHOP, PIECE_QUEEN, PIECE_KING, PIECE_BISHOP, PIECE_KNIGHT, PIECE_ROOK};
+
+    white_cord = from_cords(0, 0);
+    black_cord = from_cords(0, GRID_SIZE - 1);
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        board->piece_id[white_cord] = bottom_rank[i];
+        board->piece_id[black_cord] = bottom_rank[i];
+
+        board->piece_color[white_cord] = PLAYER_WHITE;
+        board->piece_color[black_cord] = PLAYER_BLACK;
+
+        board->piece_present[white_cord++] = true;
+        board->piece_present[black_cord++] = true;
+    }
+
+    /* --------------- Bottom Rank --------------- */
 }
 
 void board_complete_move(const struct chess_board *board, struct chess_move *move)
