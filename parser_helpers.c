c27d42e0391fb16c1b14ad5d41574bb2af17d59f
Alexander Szura <aslszura@gmail.com>
2025-11-27 11:47:54 -0500

    Revert "Merge branch 'main' into AS-Moves"
    This reverts commit 10839a72a68560c1b171d1c0a728a1253cf74c18, reversing
    changes made to aaea593bf76244bfa6b72d017bb73363cd7bcad1.

--- parser_helpers.c
+++ parser_helpers.c
@@ -1,32 +1,20 @@
 #include "parser.h"
 #include <stdio.h>
 
-/*
-*Helper functions for use & organization in main parser ("parser.c"). 
-
--> booleans to check if input is a file, rank, piece.
--> more helpers to handle specific cases like promotion, capture, castling, disambiguation.
--> other helpers for skipping spaces and struct field resetting.
-
-*/
-
-// checks if character is a valid file. ('a' to 'h)
 bool is_file(char c) {
     if (c >= 'a' and c <= 'h') {
-        return true; // valid file
+        return true;
     }
    return false;
 }
 
-// checks if character is a valid rank. (1 to 8)
 bool is_rank(char c) {
     if (c >= '1' and c <= '8') {
-        return true; // valid rank
+        return true;
     }
     return false;
 }
 
-// checks if character is a chess piece; letters correspond to different moves. (N - knight, K - king, etc)
 bool is_piece(char c) {
     switch (c) {
         case 'N':
@@ -40,30 +28,26 @@ bool is_piece(char c) {
     }
 }
 
-// skips whitespace characters in input. 
 void skip_spaces() {
     int c;
     while ((c = getc(stdin)) == ' ') {
-        ; // read chararacters as long as they are spaces.
+        ; 
     }
     if (c != EOF) {
-        ungetc(c, stdin); // if parsing stopped because of a non-EOF character, put it back. 
+        ungetc(c, stdin);
     }
 }
 
-// read the next character and verify it matches 'expected'; trigger a parse error if there isn't a match.
 void expect_char(char expected) {
     char c = getc(stdin);
     if (c != expected) {
-        parse_error(c, "expected"); // report mismatch.
+        parse_error(c, "expected");
     }
 }
 
-// parse a chessboard square (file+rank, e.g., "e4")
 int parse_square(char pre_file) {
     char file, rank;
     
-    // use provided file, otherwise read file from input.
     if (pre_file) {
         file = pre_file;
         rank = getc(stdin);
@@ -72,15 +56,12 @@ int parse_square(char pre_file) {
         rank = getc(stdin);
     }
 
-    // validate file & rank.
     if (!is_file(file)) parse_error(file, "square->file");
     if (!is_rank(rank)) parse_error(rank, "square->rank");
     
-    return (rank - '1') * 8 + (file - 'a'); // return the index converted file and rank.
+    return (rank - '1') * 8 + (file - 'a');
 }
 
-// handle flagging for parsing for capture scenarios, called when 'x' is encountered. 
-// returns true if capture was found, otherwise returns false and pushes back the character. 
 bool parse_capture() {
     char c = getc(stdin);
     if (c == 'x') {
@@ -90,18 +71,15 @@ bool parse_capture() {
     return false;
 }
 
-// handle parsing for promotion scenarios, called when the promotion symbol '=' is encountered. 
-// returns the ID of the promoted piece, or -1 if no promotion exists. 
 int parse_promotion() {
     char c = getc(stdin);
-    if (c != '=') { // check for '=' sign (promotion symbol).
+    if (c != '=') {
         ungetc(c, stdin);
         return -1;
     }
-    c = getc(stdin); // read the piece to be promoted.
-    if (!is_piece(c) or c == 'K') parse_error(c, "pawn-promote"); // promotion must be to a non-king piece.
+    c = getc(stdin);
+    if (!is_piece(c) or c == 'K') parse_error(c, "pawn-promote"); 
 
-    // return the corresponding internal piece ID.
     switch(c) {
         case 'Q': return PIECE_QUEEN;
         case 'R': return PIECE_ROOK;
@@ -111,11 +89,7 @@ int parse_promotion() {
     return -1; 
 }
 
-// handle parsing for castling scenario, called when the castling symbols 'O-O' 'O-O-O' are encountered.
-// updated move fields accordingly. 
 void parse_castle(struct chess_move *move) {
-    
-    // castling symbol. (O, then '-', ....)
     char c = getc(stdin);
     if (c != 'O') {
         parse_error(c, "expected 'O'");
@@ -143,7 +117,7 @@ void parse_castle(struct chess_move *move) {
         }
         move->is_long_castle = true;
     } else {
-        ungetc(c, stdin); // not another '-', so it's short castle.
+        ungetc(c, stdin);
         move->is_long_castle = false;
     }
     
@@ -152,7 +126,6 @@ void parse_castle(struct chess_move *move) {
     move->to_square = -1;  
 }
 
-// resets all fields in chess_move object to default state. 
 void reset_fields(struct chess_move *move) {
     move->is_capture = false;
     move->is_castle = false;
@@ -165,38 +138,30 @@ void reset_fields(struct chess_move *move) {
     move->from_rank = '\0';
 }
 
-// parse optional piece disambiguation. (file/rank)
-// used in moves like "Nbd2" (ambiguation is present);
-// writes result to from_file & from_rank (pointers) and returns the number of characters consumed from the input. 
 int parse_disambiguation(char *from_file, char *from_rank) {
-    int c_consumed = 0; // storage for number of consumed characters. 
+    int c_consumed = 0;
     
     int c = getc(stdin);
     if (c == EOF) return 0;
     
-    // if disambiguation starts with the capture symbol 'x', it is not disambiguation at all.
     if (c == 'x') {
         ungetc(c, stdin);
         return 0;
     }
     
-    // first case: disambiguation by file. 
     if (is_file(c)) {
-        // peek at next character without consuming.
         int next_c = getc(stdin);
         ungetc(next_c, stdin);
         ungetc(c, stdin);
 
-        if (is_rank(next_c)) { // the parser will consume if "file + rank" so do nothing. 
+        if (is_rank(next_c)) {
             return 0;
         }
         
-        // otherwise, consume the file disambiguation.
         c = getc(stdin); 
         if (from_file) *from_file = c;
-        c_consumed++; // update storage/counter. 
+        c_consumed++;
         
-        // look for optional rank disambiguation.
         c = getc(stdin);
         if (is_rank(c)) {
             if (from_rank) *from_rank = c;
@@ -205,7 +170,6 @@ int parse_disambiguation(char *from_file, char *from_rank) {
             ungetc(c, stdin);
         }
     }
-    // second case: disambiguation by rank only. 
     elif (is_rank(c)) {
         if (from_rank) *from_rank = c;
         c_consumed++;
